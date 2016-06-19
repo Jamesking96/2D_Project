bc50de38ba91d55694a55d3c244f9862a80fcdb4
WASDMagician <WASDMagician@gmail.com>
2016-06-19 17:23:54 +0100

    Work transferred over
    Player movement and some attacking stuff done

---
+++ GoblynGame/Assets/Scripts/Characters/character_controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character_controller : MonoBehaviour {
+
+	public character_movement movement;
+	public Animator animator;
+	public weapon_base weapon;
+	public int health;
+
+	// Use this for initialization
+	void Start () {
+		movement = GetComponent <character_movement> ();
+		animator = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
